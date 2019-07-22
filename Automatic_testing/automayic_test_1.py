5e5dcff794af1d0b63dd177d5cf67afae95aa326
jijuntao <18855532272@163.com>
2019-07-23 00:27:27 +0800

    学习必备软件安装

---
+++ Automatic_testing/automayic_test_1.py
@@ -0,0 +1,7 @@
+# 安装python 3.7，百度教程
+# 安装selenium，下载网址官网，最好适应64位，在selenium目录下按住shift+鼠标右键，打开cmd，输入pyhton setup.py install
+#     验证是否安装成功输入pip list，若有selenium即成功
+# 安装火狐、谷歌浏览器
+# 安装火狐浏览器驱动geckodrive-64，浏览器驱动应放置在python安装目录下
+# 了解前端知识html，js，css，json，
+# 在python的IDLE编辑器中输入from selenium import webdriver即可使用火狐浏览器驱动
